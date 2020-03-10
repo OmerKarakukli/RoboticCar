19199b258aef262365a8f06c95fdebd0bd1d2a52
omer <omerk124@gmail.com>
2020-03-10 17:08:07 +0200

    fix bug of two users send messages at the same time -> infinit loop

--- User_server.py
+++ User_server.py
@@ -12,7 +12,6 @@ except Exception as e:
 while True:
     msg, address = sock.recvfrom(1024)
     sock.sendto(msg, ('127.0.0.1', 10000))
-    #need to add a verification that the message is from arduino
     while True:
         arduino_msg, arduino_address = sock.recvfrom(1024)
         if arduino_address[1] == 10000:
