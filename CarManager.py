56af9c8e1b726c70a3ec133f3e6d1898f1a3b60f
omer <omerk124@gmail.com>
2020-03-13 18:17:48 +0200

    update CarManager.py

--- CarManager.py
+++ CarManager.py
@@ -1,31 +1,70 @@
 import socket
 from time import sleep
+from Communication import UDP
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDP_address = ('127.0.0.1', 10002)
 
-try:
-    sock.bind(UDP_address)
-except Exception as e:
-    print(e)
-    exit()
+UDP1 = UDP(UDP_address)
+UDP1.bind()
 
-sock.settimeout(0.5)
+
+def getDist():
+    UDP1.sendto('dist', ('127.0.0.1', 10000))
+    dist_array, address = UDP1.recv()
+    dist_array = dist_array.split(',')
+    for j in range(0, len(dist_array)):
+        dist_array[j] = int(dist_array[j])
+    return dist_array
+
 
 while True:
-    sock.sendto(bytes('frontDist', 'utf-8'), ('127.0.0.1', 10000))
-    try:
-        while True:
-            frontDist, arduino_address = sock.recvfrom(1024)
-            if arduino_address[1] == 10000:
-                break
-        frontDist = int(frontDist.decode('utf-8'))
-        print(frontDist)
-        if frontDist < 250:
-            sock.sendto(bytes('a', 'utf-8'), ('127.0.0.1', 10000))
-            sleep(1)
+    # sleep(0.01)
+    dist = getDist()
+    print(dist)
+    if dist[1] < 250 or dist[2] < 250:
+        if dist[0] <= dist[3]:
+            UDP1.sendto('d', ('127.0.0.1', 10000))
+            UDP1.recv()
+            print("turn Right")
         else:
-            sock.sendto(bytes('w', 'utf-8'), ('127.0.0.1', 10000))
-    except Exception as e:
-        print(e)
+            UDP1.sendto('a', ('127.0.0.1', 10000))
+            UDP1.recv()
+            print("turn Left")
+
+    elif dist[0] < 250:
+        UDP1.sendto('d', ('127.0.0.1', 10000))
+        UDP1.recv()
+        print("turn Right")
+
+    elif dist[3] < 250:
+        UDP1.sendto('a', ('127.0.0.1', 10000))
+        UDP1.recv()
+        print("turn Left")
+
+    else:
+        UDP1.sendto('w', ('127.0.0.1', 10000))
+        UDP1.recv()
+        print("go Forward")
+
+
+
+
+
+
+# while True:
+#     # sock.sendto(bytes('frontDist', 'utf-8'), ('127.0.0.1', 10000))
+#     try:
+#         while True:
+#             frontDist, arduino_address = sock.recvfrom(1024)
+#             if arduino_address[1] == 10000:
+#                 break
+#         frontDist = int(frontDist.decode('utf-8'))
+#         print(frontDist)
+#         if frontDist < 250:
+#             sock.sendto(bytes('a', 'utf-8'), ('127.0.0.1', 10000))
+#             sleep(1)
+#         else:
+#             sock.sendto(bytes('w', 'utf-8'), ('127.0.0.1', 10000))
+#     except Exception as e:
+#         print(e)
 
